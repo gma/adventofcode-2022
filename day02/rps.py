6de03aacfe00575898f65f643d64a06e3173e337
Graham Ashton <graham@effectif.com>
2022-12-02 09:33:37 +0000

    Day 2 part 1: Calculate score for a win
    Note that we don't need any extra code (or tests) in order to calculate
    the score for a loss, as the current implementation is correct. i.e. You
    only score for the shape you played.

--- day02/rps.py
+++ day02/rps.py
@@ -11,8 +11,17 @@ moves = {
 shape_scores = {"rock": 1, "paper": 2, "scissors": 3}
 
 
+winning_games = (
+    ("rock", "paper"),
+    ("paper", "scissors"),
+    ("scissors", "rock"),
+)
+
+
 def score_for_round(opponent, player):
     score = shape_scores[moves[player]]
     if moves[opponent] == moves[player]:
         score += 3
+    elif (moves[opponent], moves[player]) in winning_games:
+        score += 6
     return score
--- day02/test_rps.py
+++ day02/test_rps.py
@@ -1,10 +1,19 @@
 import rps
 
 
+rock_score = 1
+paper_score = 2
+scissors_score = 3
+
+
 def test_calculate_score_for_drawn_round():
-    rock_score = 1
-    scissors_score = 3
     draw_score = 3
 
     assert rps.score_for_round("A", "X") == rock_score + draw_score
     assert rps.score_for_round("C", "Z") == scissors_score + draw_score
+
+
+def test_calculate_score_for_win():
+    win_score = 6
+
+    assert rps.score_for_round("A", "Y") == paper_score + win_score
