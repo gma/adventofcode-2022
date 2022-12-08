3eed11a22c9bd5dfb73c0704c35fcd8dfa549853
Graham Ashton <graham@effectif.com>
2022-12-08 09:42:45 +0000

    Day 8 part 1: Calculate number of visible trees

--- day08/test_trees.py
+++ day08/test_trees.py
@@ -18,6 +18,18 @@ def test_load_grid_returns_sequence_of_sequences():
     assert trees.load_grid(io.StringIO("12\n34\n")) == [[1, 2], [3, 4]]
 
 
-@pytest.mark.skip
+class TestVisibility:
+    def test_tree_is_visible_when_taller_than_trees_to_its_left(self):
+        assert trees.visible_from_start(1, sequence=[1, 2, 2])
+
+    def test_tree_is_visible_when_taller_than_trees_to_its_right(self):
+        assert trees.visible_from_end(1, sequence=[2, 2, 1])
+
+    def test_visible_internal_trees_finds_the_right_trees(self):
+        grid = trees.load_grid(io.StringIO(test_data))
+
+        assert len(trees.visible_trees(grid)) == 21
+
+
 def test_part1_solution():
     assert trees.part1(io.StringIO(test_data)) == 21
--- day08/trees.py
+++ day08/trees.py
@@ -2,6 +2,32 @@ def load_grid(file):
     return [[int(height) for height in line.strip()] for line in file]
 
 
+def visible_from_start(i, sequence):
+    return all(sequence[i] > height for height in sequence[:i])
+
+
+def visible_from_end(i, sequence):
+    return all(sequence[i] > height for height in sequence[i + 1 :])
+
+
+def visible_trees(grid):
+    visible = []
+    for y, row in enumerate(grid):
+        for x in range(len(row)):
+            col = [r[x] for r in grid]
+            if (
+                visible_from_start(x, row)
+                or visible_from_end(x, row)
+                or visible_from_start(y, col)
+                or visible_from_end(y, col)
+            ):
+                visible.append((x, y))
+    return visible
+
+
 def part1(file):
-    plantation = load_grid(file)
-    return num_edge_trees(plantation) + len(visible_internal_trees(plantation))
+    return len(visible_trees(load_grid(file)))
+
+
+if __name__ == "__main__":
+    print(part1(open("input")))
